2c6baa04d975016e5b44d477e7a8a99a68f1695e
khanh <khanh.nguyen.contact@gmail.com>
2025-07-04 03:53:52 +0800

    auto_push

--- pkg/el/expr/parser.go
+++ pkg/el/expr/parser.go
@@ -4,41 +4,6 @@ import (
 	"errors"
 )
 
-// Expr : union of Name, Lambda
-type Expr interface {
-	String() string
-	MustTypeExpr() // for type-safety every Expr must implement this
-}
-
-// Name - a name, a number, a string, etc.
-type Name string
-
-func (e Name) String() string {
-	return string(e)
-}
-
-func (e Name) MustTypeExpr() {}
-
-// Lambda - S-expression - every enclosed by a pair of parentheses
-// e.g. (cmd ...)
-type Lambda struct {
-	Cmd  Expr
-	Args []Expr
-}
-
-func (e Lambda) String() string {
-	s := ""
-	s += "("
-	s += e.Cmd.String()
-	for _, arg := range e.Args {
-		s += " " + arg.String()
-	}
-	s += ")"
-	return s
-}
-
-func (e Lambda) MustTypeExpr() {}
-
 func pop(tokenList []Token) ([]Token, Token, error) {
 	if len(tokenList) == 0 {
 		return nil, "", errors.New("empty token list")
