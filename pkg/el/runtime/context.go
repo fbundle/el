73e54787c4b59ebf49da6373e74aa064ed855974
khanh <khanh.nguyen.contact@gmail.com>
2025-07-09 00:16:55 +0800

    auto_push

--- pkg/el/runtime/context.go
+++ pkg/el/runtime/context.go
@@ -2,11 +2,15 @@ package runtime
 
 import "context"
 
+type tailcallCtxKey struct{}
+
+var tailcallCtxKeyVal = tailcallCtxKey{}
+
 func setTailCall(ctx context.Context) context.Context {
-	return context.WithValue(ctx, "tailcall", true)
+	return context.WithValue(ctx, tailcallCtxKeyVal, true)
 }
 
 func getTailCall(ctx context.Context) bool {
-	v, ok := ctx.Value("tailcall").(bool)
+	v, ok := ctx.Value(tailcallCtxKeyVal).(bool)
 	return ok && v
 }
