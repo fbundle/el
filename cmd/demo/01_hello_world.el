93cb9e9f55a933c15e9d3837277422a03004dfee
khanh <khanh.nguyen.contact@gmail.com>
2025-09-04 01:02:51 +0800

    auto_push

--- cmd/demo/01_hello_world.el
+++
@@ -1,9 +0,0 @@
-# Hello World Demo
-# This demonstrates the basic "Hello World" program in El
-
-(let
-    _ (print "Hello, World!")
-    _ (print "Welcome to El programming language!")
-    _ (print "This is a functional Lisp-like language")
-    nil
-)
