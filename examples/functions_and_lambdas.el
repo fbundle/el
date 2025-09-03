2d012e538b7f488b46e8995936044b587d71a7b0
khanh <khanh.nguyen.contact@gmail.com>
2025-09-04 02:20:52 +0800

    auto_push

--- examples/functions_and_lambdas.el
+++ examples/functions_and_lambdas.el
@@ -1,107 +1,101 @@
-# Functions and Lambda Expressions
-_ (print "=== Functions and Lambdas ===")
+(let
+    # Functions and Lambda Expressions
+    _ (print "=== Functions and Lambdas ===")
 
-# Simple lambda functions
-_ (let
-    square (lambda x {x x x})
-    _ (print "Square of 5:" (square 5))
+    # Simple lambda functions
+    _ (let
+        square (lambda n {n x n})
+        _ (print "Square of 5:" (square 5))
+        nil
+    )
+
+    # Arrow function syntax
+    _ (let
+        cube {n => {n x n x n}}
+        _ (print "Cube of 3:" (cube 3))
+        nil
+    )
+
+    # Multi-parameter functions
+    _ (let
+        add (lambda a b {a + b})
+        _ (print "Add 10 and 20:" (add 10 20))
+        nil
+    )
+
+    # Higher-order functions
+    _ (let
+        square (lambda a {a x a})
+        apply_twice (lambda f a (f (f a)))
+        _ (print "Apply square twice to 2:" (apply_twice square 2))
+        nil
+    )
+
+    # Function composition
+    _ (let
+        square (lambda a {a x a})
+        cube {a => {a x a x a}}
+        compose {f g => {x => (f (g x))}}
+        compose_square_cube (compose square cube)
+        _ (print "Compose square and cube of 2:" (compose_square_cube 2))
+        nil
+    )
+
+    # Recursive functions
+    _ (let
+        factorial (lambda n (match {n <= 1}
+            true 1
+            {n x (factorial {n - 1})}
+        ))
+        _ (print "Factorial of 5:" (factorial 5))
+        nil
+    )
+
+    # Fibonacci with memoization concept
+    _ (let
+        fib (lambda n (match {n <= 1}
+            true n
+            (let
+                p (fib {n - 1})
+                q (fib {n - 2})
+                {p + q}
+            )
+        ))
+        _ (print "Fibonacci of 10:" (fib 10))
+        nil
+    )
+
+    # Mutual recursion
+    _ (let
+        even (lambda n (match {n <= 0}
+            true true
+            (odd {n - 1})
+        ))
+        odd (lambda n (match {n <= 0}
+            true false
+            (even {n - 1})
+        ))
+        _ (print "Even/Odd test:" [(even 10) (odd 10) (even 11) (odd 11)])
+        nil
+    )
+
+    # Closure example
+    _ (let
+        make_counter (lambda start (lambda {start + 1}))
+        counter (make_counter 0)
+        _ (print "Counter values:" [(counter) (counter) (counter)])
+        nil
+    )
+
+    # Function that returns functions
+    _ (let
+        make_multiplier (lambda factor (lambda a {a x factor}))
+        double (make_multiplier 2)
+        triple (make_multiplier 3)
+        _ (print "Double 7:" (double 7))
+        _ (print "Triple 7:" (triple 7))
+        nil
+    )
+
     nil
 )
-
-# Arrow function syntax
-_ (let
-    cube {x => {x x x x}}
-    _ (print "Cube of 3:" (cube 3))
-    nil
-)
-
-# Multi-parameter functions
-_ (let
-    add (lambda x y {x + y})
-    _ (print "Add 10 and 20:" (add 10 20))
-    nil
-)
-
-# Higher-order functions
-_ (let
-    square (lambda x {x x x})
-    apply_twice (lambda f x (f (f x)))
-    _ (print "Apply square twice to 2:" (apply_twice square 2))
-    nil
-)
-
-# Function composition
-_ (let
-    square (lambda x {x x x})
-    cube {x => {x x x x}}
-    compose_square_cube (compose square cube)
-    _ (print "Compose square and cube of 2:" (compose_square_cube 2))
-    nil
-)
-
-# Currying
-_ (let
-    add (lambda x y {x + y})
-    curried_add (curry add)
-    add_5 (curried_add 5)
-    _ (print "Curried add 5 to 10:" (add_5 10))
-    nil
-)
-
-# Recursive functions
-_ (let
-    factorial (lambda n (match {n <= 1}
-        true 1
-        {n x (factorial {n - 1})}
-    ))
-    _ (print "Factorial of 5:" (factorial 5))
-    nil
-)
-
-# Fibonacci with memoization concept
-_ (let
-    fib (lambda n (match {n <= 1}
-        true n
-        (let
-            p (fib {n - 1})
-            q (fib {n - 2})
-            {p + q}
-        )
-    ))
-    _ (print "Fibonacci of 10:" (fib 10))
-    nil
-)
-
-# Mutual recursion
-_ (let
-    even (lambda n (match {n <= 0}
-        true true
-        (odd {n - 1})
-    ))
-    odd (lambda n (match {n <= 0}
-        true false
-        (even {n - 1})
-    ))
-    _ (print "Even/Odd test:" [(even 10) (odd 10) (even 11) (odd 11)])
-    nil
-)
-
-# Closure example
-_ (let
-    make_counter (lambda start (lambda {start + 1}))
-    counter (make_counter 0)
-    _ (print "Counter values:" [(counter) (counter) (counter)])
-    nil
-)
-
-# Function that returns functions
-_ (let
-    make_multiplier (lambda factor (lambda x {x x factor}))
-    double (make_multiplier 2)
-    triple (make_multiplier 3)
-    _ (print "Double 7:" (double 7))
-    _ (print "Triple 7:" (triple 7))
-    nil
-)
-
-nil
--- examples/pattern_matching.el
+++ examples/pattern_matching.el
@@ -1,87 +1,89 @@
-# Pattern Matching and Conditionals
-_ (print "=== Pattern Matching ===")
+(let
+    # Pattern Matching and Conditionals
+    _ (print "=== Pattern Matching ===")
 
-# Basic pattern matching
-_ (print "Match 1 == 1:" (match 1 1 "yes" "no"))
-_ (print "Match 1 == 2:" (match 1 2 "yes" "no"))
+    # Basic pattern matching
+    _ (print "Match 1 == 1:" (match 1 1 "yes" "no"))
+    _ (print "Match 1 == 2:" (match 1 2 "yes" "no"))
 
-# Multiple conditions
-_ (let
-    grade_check (lambda score (match score
-        100 "Perfect!"
-        90 "Excellent"
-        80 "Good"
-        70 "Average"
-        60 "Below Average"
-        "Fail"
-    ))
-    _ (print "Grade for 95:" (grade_check 95))
-    _ (print "Grade for 75:" (grade_check 75))
-    _ (print "Grade for 45:" (grade_check 45))
+    # Multiple conditions
+    _ (let
+        grade_check (lambda score (match score
+            100 "Perfect!"
+            90 "Excellent"
+            80 "Good"
+            70 "Average"
+            60 "Below Average"
+            "Fail"
+        ))
+        _ (print "Grade for 95:" (grade_check 95))
+        _ (print "Grade for 75:" (grade_check 75))
+        _ (print "Grade for 45:" (grade_check 45))
+        nil
+    )
+
+    # Boolean operations
+    _ (print "True and True:" (and true true))
+    _ (print "True and False:" (and true false))
+    _ (print "False or True:" (or false true))
+    _ (print "Not True:" (not true))
+
+    # Conditional expressions
+    _ (print "If 5 > 3:" (if {5 > 3} "greater" "not greater"))
+    _ (print "If 2 > 5:" (if {2 > 5} "greater" "not greater"))
+
+    # Complex pattern matching with calculations
+    _ (let
+        abs (lambda x (match {x < 0}
+            true {-x}
+            x
+        ))
+        _ (print "Absolute value of -5:" (abs -5))
+        _ (print "Absolute value of 5:" (abs 5))
+        nil
+    )
+
+    # String pattern matching
+    _ (let
+        greet (lambda name (match name
+            "Alice" "Hello Alice!"
+            "Bob" "Hi Bob!"
+            "Hello stranger!"
+        ))
+        _ (print "Greet Alice:" (greet "Alice"))
+        _ (print "Greet Bob:" (greet "Bob"))
+        _ (print "Greet Charlie:" (greet "Charlie"))
+        nil
+    )
+
+    # List pattern matching
+    _ (let
+        list_length (lambda lst (match (len lst)
+            0 0
+            (let
+                rest_lst (rest lst)
+                {1 + (list_length rest_lst)}
+            )
+        ))
+        _ (print "Length of [1 2 3 4]:" (list_length [1 2 3 4]))
+        _ (print "Length of []:" (list_length []))
+        nil
+    )
+
+    # Type checking with pattern matching
+    _ (let
+        type_check (lambda x (match (type x)
+            "number" "It's a number"
+            "string" "It's a string"
+            "list" "It's a list"
+            "function" "It's a function"
+            "unknown type"
+        ))
+        _ (print "Type of 42:" (type_check 42))
+        _ (print "Type of 'hello':" (type_check "hello"))
+        _ (print "Type of [1 2 3]:" (type_check [1 2 3]))
+        nil
+    )
+
     nil
 )
-
-# Boolean operations
-_ (print "True and True:" (and true true))
-_ (print "True and False:" (and true false))
-_ (print "False or True:" (or false true))
-_ (print "Not True:" (not true))
-
-# Conditional expressions
-_ (print "If 5 > 3:" (if {5 > 3} "greater" "not greater"))
-_ (print "If 2 > 5:" (if {2 > 5} "greater" "not greater"))
-
-# Complex pattern matching with calculations
-_ (let
-    abs (lambda x (match {x < 0}
-        true {-x}
-        x
-    ))
-    _ (print "Absolute value of -5:" (abs -5))
-    _ (print "Absolute value of 5:" (abs 5))
-    nil
-)
-
-# String pattern matching
-_ (let
-    greet (lambda name (match name
-        "Alice" "Hello Alice!"
-        "Bob" "Hi Bob!"
-        "Hello stranger!"
-    ))
-    _ (print "Greet Alice:" (greet "Alice"))
-    _ (print "Greet Bob:" (greet "Bob"))
-    _ (print "Greet Charlie:" (greet "Charlie"))
-    nil
-)
-
-# List pattern matching
-_ (let
-    list_length (lambda lst (match (len lst)
-        0 0
-        (let
-            rest_lst (rest lst)
-            {1 + (list_length rest_lst)}
-        )
-    ))
-    _ (print "Length of [1 2 3 4]:" (list_length [1 2 3 4]))
-    _ (print "Length of []:" (list_length []))
-    nil
-)
-
-# Type checking with pattern matching
-_ (let
-    type_check (lambda x (match (type x)
-        "number" "It's a number"
-        "string" "It's a string"
-        "list" "It's a list"
-        "function" "It's a function"
-        "unknown type"
-    ))
-    _ (print "Type of 42:" (type_check 42))
-    _ (print "Type of 'hello':" (type_check "hello"))
-    _ (print "Type of [1 2 3]:" (type_check [1 2 3]))
-    nil
-)
-
-nil
