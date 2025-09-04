35dab025cab9bf09d316f1ef64500dc5a5445311
khanh <khanh.nguyen.contact@gmail.com>
2025-09-05 00:06:03 +0800

    auto_push

--- cmd/ts/main.go
+++ cmd/ts/main.go
@@ -32,18 +32,63 @@ func makeData(data ts.Data, dtype ts.Sort) ts.Sort {
 
 func printSorts(sorts ...ts.Sort) {
 	for _, sort := range sorts {
-		fmt.Printf("%s \t--- type %s\n", sort.String(), sort.Type().String())
+		fmt.Printf("[%s] is of type [%s]\n", sort.String(), sort.Type().String())
+	}
+}
+func printCast(value1 ts.Sort, type2 ts.Sort) {
+	ok := value1.Cast(type2).Ok
+
+	if ok {
+		fmt.Printf("value [%s] of type [%s] CAN be cast into [%s]\n", value1, value1.Type(), type2)
+	} else {
+		fmt.Printf("value [%s] of type [%s] CANNOT be cast into [%s]\n", value1, value1.Type(), type2)
 	}
 }
 
+func strongestType(length int) ts.Sort {
+	// this type can be cast into every type of this length
+	if length < 1 {
+		panic("type_error")
+	}
+	var sorts []ts.Sort
+	for i := 0; i < length-1; i++ {
+		sorts = append(sorts, ts.MustSingleName(typeLevel, ts.Initial))
+	}
+	sorts = append(sorts, ts.MustSingleName(typeLevel, ts.Final))
+
+	return ts.MustChain(sorts...)
+}
+
+func weakestType(length int) ts.Sort {
+	// every type of this length can be cast into this type
+	if length < 1 {
+		panic("type_error")
+	}
+	var sorts []ts.Sort
+	for i := 0; i < length-1; i++ {
+		sorts = append(sorts, ts.MustSingleName(typeLevel, ts.Final))
+	}
+	sorts = append(sorts, ts.MustSingleName(typeLevel, ts.Initial))
+
+	return ts.MustChain(sorts...)
+}
+
 func main() {
+	fmt.Printf("anything can be cast into [%s]\n", ts.Final)
+	fmt.Printf("[%s] can be cast into anything\n", ts.Initial)
+
 	intType := makeNameType("int")
 	boolType := makeNameType("bool")
 	stringType := makeNameType("string")
 	intIntType := ts.MustChain(intType, intType)
 	intIntIntType := ts.MustChain(intType, intType, intType)
+	weak1 := weakestType(1)
+	strong1 := strongestType(1)
+	weak3 := weakestType(3)
+	strong3 := strongestType(3)
 
 	ts.AddRule("bool", "int") // cast bool -> int
+	fmt.Println("[bool] can be cast into [int]")
 
 	oneSort := makeData(newData(1), intType)
 	trueSort := makeData(newData(true), boolType)
@@ -57,6 +102,10 @@ func main() {
 		}
 	}), intIntIntType)
 
-	printSorts(oneSort, trueSort, helloSort, add1, add)
+	printSorts(oneSort, trueSort, helloSort, add1, add, weak1, strong1, weak3, strong3)
 
+	printCast(trueSort, intType)
+	printCast(oneSort, boolType)
+	printCast(add, weak3)
+	printCast(add, strong3)
 }
