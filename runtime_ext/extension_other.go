fcb6043e8a4c0ec4bf76123a0a1e204a22385f7d
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-04 11:45:27 +0800

    auto_push

--- runtime_ext/extension_other.go
+++ runtime_ext/extension_other.go
@@ -2,6 +2,7 @@ package runtime_ext
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/fbundle/lab_public/lab/go_util/pkg/adt"
@@ -24,8 +25,14 @@ var printExtension = Extension{
 
 var typeExtension = Extension{
 	Name: "type",
-	Man:  "{builtin: (print 1 2 (lambda x (add x 1))) - get type in list of strings}",
+	Man:  "{builtin: (print 1 2 (lambda x (add x 1))) - get type in string}",
 	Exec: func(ctx context.Context, values ...Object) adt.Result[Object] {
+		return errValue(errors.New("not implemented"))
+		/*
+			- add Type() into Object interface
 
+			1 						-> "object" : integer
+			(lambda x y {x + y}) 	-> "func2" : function of 2 arguments
+		*/
 	},
 }
