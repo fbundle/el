3af36269ea4e888b9dc6b036702c38a6209681f4
khanh <khanh.nguyen.contact@gmail.com>
2025-09-05 02:35:23 +0800

    auto_push

--- cmd/ts/main.go
+++ cmd/ts/main.go
@@ -10,7 +10,7 @@ const (
 )
 
 func makeNameType(typeName string) ts.Sort {
-	return ts.MustSingle(typeLevel, typeName)
+	return ts.MustSingle(typeLevel, typeName, nil)
 }
 
 func printSorts(sorts ...ts.Sort) {
@@ -35,9 +35,9 @@ func strongestType(length int) ts.Sort {
 	}
 	var sorts []ts.Sort
 	for i := 0; i < length-1; i++ {
-		sorts = append(sorts, ts.MustSingle(typeLevel, ts.Initial))
+		sorts = append(sorts, ts.MustSingle(typeLevel, ts.Initial, nil))
 	}
-	sorts = append(sorts, ts.MustSingle(typeLevel, ts.Final))
+	sorts = append(sorts, ts.MustSingle(typeLevel, ts.Final, nil))
 
 	return ts.MustChain(sorts...)
 }
@@ -49,9 +49,9 @@ func weakestType(length int) ts.Sort {
 	}
 	var sorts []ts.Sort
 	for i := 0; i < length-1; i++ {
-		sorts = append(sorts, ts.MustSingle(typeLevel, ts.Final))
+		sorts = append(sorts, ts.MustSingle(typeLevel, ts.Final, nil))
 	}
-	sorts = append(sorts, ts.MustSingle(typeLevel, ts.Initial))
+	sorts = append(sorts, ts.MustSingle(typeLevel, ts.Initial, nil))
 
 	return ts.MustChain(sorts...)
 }
