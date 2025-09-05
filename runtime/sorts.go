37a2f35cb597f0ea4873879d6ca02bb7d8dab1a3
khanh <khanh.nguyen.contact@gmail.com>
2025-09-05 19:28:58 +0800

    auto_push

--- runtime/sorts.go
+++ runtime/sorts.go
@@ -19,26 +19,23 @@ var Arrow = sorts.Arrow
 
 func MakeType(name string) Object {
 	o := _object{
-		data:   nil,
-		sort:   sorts.MustAtom(_typeLevel, name, nil),
-		parent: nil, // default parent
+		data: nil,
+		sort: sorts.MustAtom(_typeLevel, name, nil),
 	}
 	return o
 }
 
 func MakeData(data Data, parent Object) Object {
 	return _object{
-		data:   data,
-		sort:   sorts.MustAtom(_dataLevel, data.String(), parent.Sort()),
-		parent: parent.Sort(),
+		data: data,
+		sort: sorts.MustAtom(_dataLevel, data.String(), parent.Sort()),
 	}
 }
 
 func MakeSort(sort Sort) Object {
 	return _object{
-		data:   nil, // no data
-		sort:   sort,
-		parent: nil, // default parent
+		data: nil, // no data
+		sort: sort,
 	}
 }
 
@@ -56,18 +53,16 @@ func makeWeakestType(numParams int) Object {
 
 	// convert sort to object
 	return _object{
-		data:   nil,
-		sort:   s,
-		parent: nil,
+		data: nil,
+		sort: s,
 	}
 
 }
 
 // _object - unorder-score means private, even in the same package
 type _object struct {
-	data   Data // nullable
-	sort   Sort
-	parent Sort
+	data Data // nullable
+	sort Sort // hold the sort of object
 }
 
 func (o _object) Data() Data {
@@ -82,14 +77,13 @@ func (o _object) Sort() Sort {
 }
 func (o _object) Type() Object {
 	return _object{
-		data:   nil,
-		sort:   o.parent,
-		parent: o.parent.Parent(),
+		data: nil,
+		sort: o.sort.Parent(),
 	}
 }
 func (o _object) Cast(newParent Object) adt.Option[Object] {
 	newParentObject := newParent.(_object) // must cast
-	if ok := o.parent.LessEqual(newParentObject.sort); !ok {
+	if ok := o.sort.Parent().LessEqual(newParentObject.sort); !ok {
 		return adt.None[Object]()
 	}
 
